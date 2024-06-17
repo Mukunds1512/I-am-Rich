14a953df8cd2f431b4ae178e1c69723cab05a2a1
Mukunds1512 <mukunds1512@gmail.com>
2024-06-17 12:03:37 -0400

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Mukund Sharma on 5/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
